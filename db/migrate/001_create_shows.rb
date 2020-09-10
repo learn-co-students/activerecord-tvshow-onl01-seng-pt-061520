ad8e5b314acdff49afa235b45aba26059807ce38
Cameron Hudson <camnn3@yahoo.com>
2020-09-10 01:49:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,4 +2,7 @@ class CreateShows < ActiveRecord::Migration[5.2]
   
   def change
     create_table :shows do |s|
-      
+      s.string :name
+      s.string :day
+      s.string :network
+      s.integer :rating
